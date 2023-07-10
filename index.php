d3ae4f798df19a38f7d228868fd0e93f76481173
stolentino <stotino2000@yahoo.com>
2023-07-09 21:14:02 -0700

    add autoloading

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require_once "src/config.php";
+$content = new Collection();
 $title = "My Website";
 
 require 'views/header.php';
---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+
+function autoloader($class_name){
+    foreach(glob(__DIR__ . '/*', GLOB_ONLYDIR) as $dir){
+        if(file_exists("$dir/" . $class_name . '.php')) {
+            require_once "$dir/" . $class_name . '.php';
+            break;
+        }
+    }
+}
+
+spl_autoload_register('autoloader');
