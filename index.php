254c6c3203e5640cf8a018f860a1c80d89831550
stolentino <stotino2000@yahoo.com>
2023-08-19 14:47:56 -0700

    extend the interface

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ require 'views/header.php';
 //var_dump($repo->find('posts', 1));
 
 foreach ($content as $item){
-    echo $item->title;
+    //echo $item->title;
+    include 'views/list.php';
 }
 
 require 'views/footer.php';
--- src/classes/Collection.php
+++ src/classes/Collection.php
@@ -1,6 +1,6 @@
 <?php
 
-class Collection implements Iterator, Countable{
+class Collection implements CollectionInterface{
     protected $repo;
     public $collection;
 
@@ -13,28 +13,42 @@ class Collection implements Iterator, Countable{
         }
     }
 
+    public function shortDescription(){
+        if(strlen($this->current()->details) < 510){
+            return strip_tags($this->current()->details);
+        }
+
+        return strip_tags(
+            substr(
+                $this->current()->details,
+                0,
+                strpos($this->current()->details, ' ', 500)
+            )
+        ) . '...';
+    }
+
     public function current(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return current($this->collection);
     }
     public function key(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return key($this->collection);
     }
     public function next(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return next($this->collection);
     }
     public function rewind(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return reset($this->collection);
     }
     public function valid(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return key($this->collection) !== null;
     }
     public function count(){
-        var_dump(__METHOD__);
+        //var_dump(__METHOD__);
         return count($this->collection);
     }
 }
